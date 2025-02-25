d94c24309fa3661b1725ac18f773bcce4819cce7
Yongmin0423 <yym21345@gmail.com>
2025-02-25 23:47:18 +0900

    feat: add a news fetch

---
+++ main.js
@@ -0,0 +1,22 @@
+const API_KEY = "8b29827c21c24c2a90a7231a1761642c";
+let news = [];
+
+const getLatestNews = async () => {
+  const url = new URL(
+    `https://newsapi.org/v2/top-headlines?country=us&apiKey=${API_KEY}`
+  );
+  try {
+    const response = await fetch(url);
+
+    if (!response.ok) {
+      throw new Error(`HTTP error! Status: ${response.status}`);
+    }
+
+    const data = await response.json();
+    news = data.articles;
+  } catch (error) {
+    console.error("Error fetching news:", error);
+  }
+};
+
+getLatestNews();
