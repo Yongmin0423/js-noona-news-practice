5dff67ac350b34c0b675cc1f57a3ffcaef73f956
Yongmin0423 <yym21345@gmail.com>
2025-02-26 22:53:16 +0900

    fix: change url

--- main.js
+++ main.js
@@ -3,7 +3,8 @@ let news = [];
 
 const getLatestNews = async () => {
   const url = new URL(
-    `https://newsapi.org/v2/top-headlines?country=us&apiKey=${API_KEY}`
+    // `https://newsapi.org/v2/top-headlines?country=us&apiKey=${API_KEY}`
+    `https://study-website-be-bbb1539aa813.herokuapp.com`
   );
   try {
     const response = await fetch(url);
